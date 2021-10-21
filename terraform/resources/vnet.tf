023deff73878997466a029f40907e7d3006d2c9c
Tom Augspurger <taugspurger@microsoft.com>
2021-10-21 08:15:31 -0500

    Added network security group to subnet (#25)

--- terraform/resources/vnet.tf
+++ terraform/resources/vnet.tf
@@ -6,9 +6,21 @@ resource "azurerm_virtual_network" "pc_compute" {
   tags                = {}
 }
 
+resource "azurerm_network_security_group" "pc_compute" {
+  name                = "${local.maybe_staging_prefix}-nsg"
+  location            = azurerm_resource_group.pc_compute.location
+  resource_group_name = azurerm_resource_group.pc_compute.name
+}
+
+
 resource "azurerm_subnet" "node_subnet" {
   name                 = "${local.maybe_staging_prefix}-node-subnet"
   virtual_network_name = azurerm_virtual_network.pc_compute.name
   resource_group_name  = azurerm_resource_group.pc_compute.name
   address_prefixes     = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "pc_compute" {
+  subnet_id                 = azurerm_subnet.node_subnet.id
+  network_security_group_id = azurerm_network_security_group.pc_compute.id
 }
