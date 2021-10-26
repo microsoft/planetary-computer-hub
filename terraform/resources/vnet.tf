c2a8b40348c6a09abc4a11f5b1ee00d97bebb42f
Tom Augspurger <taugspurger@microsoft.com>
2021-10-26 12:36:31 -0500

    Added NSG
    For compliance with a Microsoft security policy.

--- terraform/resources/vnet.tf
+++ terraform/resources/vnet.tf
@@ -12,3 +12,26 @@ resource "azurerm_subnet" "node_subnet" {
   resource_group_name  = azurerm_resource_group.pc_compute.name
   address_prefixes     = ["10.1.0.0/16"]
 }
+
+resource "azurerm_network_security_group" "pc_compute" {
+  name                = "${local.maybe_staging_prefix}-security-group"
+  location            = azurerm_resource_group.pc_compute.location
+  resource_group_name = azurerm_resource_group.pc_compute.name
+
+  security_rule {
+    name                       = "hub-rule"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "pc_compute" {
+  subnet_id                 = azurerm_subnet.node_subnet.id
+  network_security_group_id = azurerm_network_security_group.pc_compute.id
+}
