682536953df07137b7d46a887189a49ec896de1d
Tom Augspurger <taugspurger@microsoft.com>
2022-01-20 10:56:12 -0600

    Update prod images

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -21,11 +21,11 @@ module "resources" {
   stac_url                  = "https://planetarycomputer.microsoft.com/api/stac/v1/"
 
   jupyterhub_singleuser_image_name = "pcccr.azurecr.io/public/planetary-computer/python"
-  jupyterhub_singleuser_image_tag  = "2021.12.29.0"
-  python_image                     = "pcccr.azurecr.io/public/planetary-computer/python:2021.12.29.0"
-  r_image                          = "pcccr.azurecr.io/public/planetary-computer/r:2021.11.19.0"
-  gpu_pytorch_image                = "pcccr.azurecr.io/public/planetary-computer/gpu-pytorch:2021.12.15.0"
-  gpu_tensorflow_image             = "pcccr.azurecr.io/public/planetary-computer/gpu-tensorflow:2021.12.15.0"
+  jupyterhub_singleuser_image_tag  = "2022.01.17.0"
+  python_image                     = "pcccr.azurecr.io/public/planetary-computer/python:2022.01.17.0"
+  r_image                          = "pcccr.azurecr.io/public/planetary-computer/r:2022.01.17.0"
+  gpu_pytorch_image                = "pcccr.azurecr.io/public/planetary-computer/gpu-pytorch:2022.01.17.0"
+  gpu_tensorflow_image             = "pcccr.azurecr.io/public/planetary-computer/gpu-tensorflow:2022.01.17.0"
   qgis_image                       = "pcccr.azurecr.io/planetary-computer/qgis:3.18.0"
 
   kbatch_proxy_url = "http://dhub-prod-kbatch-proxy.prod.svc.cluster.local"
