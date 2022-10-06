a26a0b19b56e17fe41ede12e84424e1ca2f35cf4
Tom Augspurger <taugspurger@microsoft.com>
2022-10-06 06:25:21 -0500

    Set destination_port_ranges

--- terraform/resources/vnet.tf
+++ terraform/resources/vnet.tf
@@ -25,7 +25,7 @@ resource "azurerm_network_security_group" "pc_compute" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_ranges    = ["80", "443"]
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
