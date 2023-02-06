66cb96c8831e7a36f99f4cbd439c56ef1168995a
Tom Augspurger <taugspurger@microsoft.com>
2023-02-06 08:19:27 -0600

    Updated R image

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -30,7 +30,7 @@ module "resources" {
   jupyterhub_singleuser_image_name = "pcccr.azurecr.io/public/planetary-computer/python"
   jupyterhub_singleuser_image_tag  = "2022.11.28.0"
   python_image                     = "pcccr.azurecr.io/public/planetary-computer/python:2022.11.28.0"
-  r_image                          = "pcccr.azurecr.io/public/planetary-computer/r:2022.9.8.0"
+  r_image                          = "pcccr.azurecr.io/public/planetary-computer/r:2023.1.30.0"
   gpu_pytorch_image                = "pcccr.azurecr.io/public/planetary-computer/gpu-pytorch:2022.9.16.0"
   gpu_tensorflow_image             = "pcccr.azurecr.io/public/planetary-computer/gpu-tensorflow:2022.9.16.0"
   qgis_image                       = "pcccr.azurecr.io/planetary-computer/qgis:3.18.0.1"
