690ae6d3534266b1749e4a66354438ce28e8d1b3
Tom Augspurger <taugspurger@microsoft.com>
2023-03-21 12:29:03 -0500

    Removed velero config

--- terraform/resources/keyvault.tf
+++ terraform/resources/keyvault.tf
@@ -35,28 +35,28 @@ data "azurerm_key_vault_secret" "kbatch_server_api_token" {
 
 # Velero integration
 
-data "azurerm_key_vault_secret" "velero_azure_subscription_id" {
-  name         = "${local.stack_id}--velero-azure-subscription-id"
-  key_vault_id = data.azurerm_key_vault.deploy_secrets.id
-}
+# data "azurerm_key_vault_secret" "velero_azure_subscription_id" {
+#   name         = "${local.stack_id}--velero-azure-subscription-id"
+#   key_vault_id = data.azurerm_key_vault.deploy_secrets.id
+# }
 
 
-data "azurerm_key_vault_secret" "velero_azure_tenant_id" {
-  name         = "${local.stack_id}--velero-azure-tenant-id"
-  key_vault_id = data.azurerm_key_vault.deploy_secrets.id
-}
+# data "azurerm_key_vault_secret" "velero_azure_tenant_id" {
+#   name         = "${local.stack_id}--velero-azure-tenant-id"
+#   key_vault_id = data.azurerm_key_vault.deploy_secrets.id
+# }
 
 
-data "azurerm_key_vault_secret" "velero_azure_client_id" {
-  name         = "${local.stack_id}--velero-azure-client-id"
-  key_vault_id = data.azurerm_key_vault.deploy_secrets.id
-}
+# data "azurerm_key_vault_secret" "velero_azure_client_id" {
+#   name         = "${local.stack_id}--velero-azure-client-id"
+#   key_vault_id = data.azurerm_key_vault.deploy_secrets.id
+# }
 
 
-data "azurerm_key_vault_secret" "velero_azure_client_secret" {
-  name         = "${local.stack_id}--velero-azure-client-secret"
-  key_vault_id = data.azurerm_key_vault.deploy_secrets.id
-}
+# data "azurerm_key_vault_secret" "velero_azure_client_secret" {
+#   name         = "${local.stack_id}--velero-azure-client-secret"
+#   key_vault_id = data.azurerm_key_vault.deploy_secrets.id
+# }
 
 data "azurerm_key_vault_secret" "microsoft_defender_log_analytics_workspace_id" {
   name         = "${local.stack_id}--microsoft-defender-log-analytics-workspace-id"
