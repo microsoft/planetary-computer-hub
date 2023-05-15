212106c859160ec38d75436bbbef184dd7ca3fd5
Tom Augspurger <taugspurger@microsoft.com>
2023-05-15 06:17:33 -0500

    Bump python (#87)
    (cherry picked from commit 26a0cfcbbbed217de798bad21c89fdd0eaa64230)

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -28,8 +28,8 @@ module "resources" {
   stac_url                  = "https://planetarycomputer.microsoft.com/api/stac/v1/"
 
   jupyterhub_singleuser_image_name = "pcccr.azurecr.io/public/planetary-computer/python"
-  jupyterhub_singleuser_image_tag  = "2022.9.16.0"
-  python_image                     = "pcccr.azurecr.io/public/planetary-computer/python:2022.9.16.0"
+  jupyterhub_singleuser_image_tag  = "2023.5.3.0"
+  python_image                     = "pcccr.azurecr.io/public/planetary-computer/python:2023.5.3.0"
   r_image                          = "pcccr.azurecr.io/public/planetary-computer/r:2023.1.30.0"
   gpu_pytorch_image                = "pcccr.azurecr.io/public/planetary-computer/gpu-pytorch:2022.9.16.0"
   gpu_tensorflow_image             = "pcccr.azurecr.io/public/planetary-computer/gpu-tensorflow:2022.9.16.0"
