7f1ad0091d9cbf8843fffd09bdc73d79b9c5ed69
Tom Augspurger <taugspurger@microsoft.com>
2024-03-20 22:23:38 -0500

    Bump container images

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -29,10 +29,10 @@ module "resources" {
 
   jupyterhub_singleuser_image_name = "pcccr.azurecr.io/planetary-computer/python"
   jupyterhub_singleuser_image_tag  = "2024.3.19.2"
-  python_image                     = "pcccr.azurecr.io/planetary-computer/python:2024.3.19.2"
-  r_image                          = "pcccr.azurecr.io/planetary-computer/r:2024.3.19.1"
-  gpu_pytorch_image                = "pcccr.azurecr.io/planetary-computer/gpu-pytorch:2024.3.19.3"
-  gpu_tensorflow_image             = "pcccr.azurecr.io/planetary-computer/gpu-tensorflow:2024.3.19.2"
+  python_image                     = "pcccr.azurecr.io/planetary-computer/python:2024.3.20.1"
+  r_image                          = "pcccr.azurecr.io/planetary-computer/r:2024.3.20.1"
+  gpu_pytorch_image                = "pcccr.azurecr.io/planetary-computer/gpu-pytorch:2024.3.20.1"
+  gpu_tensorflow_image             = "pcccr.azurecr.io/planetary-computer/gpu-tensorflow:2024.3.20.1"
   qgis_image                       = "pcccr.azurecr.io/planetary-computer/qgis:2024.3.19.7"
 
 }
