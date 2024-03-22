0fc2404c9175ca02120919c62fa7e03c7a8d05c0
Tom Augspurger <taugspurger@microsoft.com>
2024-03-22 15:03:53 -0500

    Fixed deployment source URL

--- terraform/resources/rg.tf
+++ terraform/resources/rg.tf
@@ -6,6 +6,6 @@ resource "azurerm_resource_group" "pc_compute" {
     component   = "hub"
     ringValue   = "r1"
 
-    deployment_source = "https://github.com/microsoft/planetary-computer-hub/"
+    deployment_source = "https://github.com/microsoft/planetary-computer-hub"
   }
 }
